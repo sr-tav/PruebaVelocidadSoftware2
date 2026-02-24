90da2789a52272c736cf946613b8e4f85d5a2438
KarolMD <karolm.davilas@uqvirtual.edu.co>
2026-02-23 20:02:00 -0500

    funcionalidad2
    LibroService y se agrego f2 al menu

--- libreria-spring/src/main/java/co/edu/uniquindio/libreriaspring/consola/MenuConsola.java
+++ libreria-spring/src/main/java/co/edu/uniquindio/libreriaspring/consola/MenuConsola.java
@@ -2,11 +2,15 @@ package co.edu.uniquindio.libreriaspring.consola;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
+import co.edu.uniquindio.libreriaspring.services.LibroService;
+import co.edu.uniquindio.libreriaspring.domain.Libro;
+
 import java.util.Scanner;
 @Component
 public class MenuConsola{
     Scanner sc = new Scanner(System.in);
     private final ConfigurableApplicationContext context;
+    private final LibroService libroService;
     String mensaje = "//////// MENU LIBRERIA //////////\n" +
             "1. consultar por autor y titulo \n" +
             "2. consultar por cualquiera \n" +
@@ -14,8 +18,9 @@ public class MenuConsola{
             "4. hacer reseña de un libro \n" +
             "0. salir \n";
 
-    public MenuConsola(ConfigurableApplicationContext context) {
+    public MenuConsola(ConfigurableApplicationContext context, LibroService libroService) {
         this.context = context;
+        this.libroService = libroService;
     }
 
     public void mostrarMenu(){
@@ -28,7 +33,7 @@ public class MenuConsola{
                     System.out.println("aqui va la logica para la prime funcionalidad");
                     break;
                 case "2":
-                    System.out.println("aqui va la logica para la segunda funcionalidad");
+                    buscarLibrosPorCriterios();
                     break;
                 case "3":
                     System.out.println("aqui va la logica para la tercer funcionalidad");
@@ -40,6 +45,32 @@ public class MenuConsola{
                     System.out.println("Saliendo del programa...");
                     context.close();
                     break;
+            }
+        }
+    }
+
+    //funcionalidad 2
+    public void buscarLibrosPorCriterios() {
+
+        System.out.println("BUSQUEDA DE LIBROS");
+
+        System.out.print("Titulo (opcional): ");
+        String titulo = sc.nextLine();
+
+        System.out.print("Autor (opcional): ");
+        String autor = sc.nextLine();
+
+        System.out.print("ISBN (opcional): ");
+        String isbn = sc.nextLine();
+
+        var resultados = libroService.buscarPorCriterios(titulo, autor, isbn);
+
+        if (resultados.isEmpty()) {
+            System.out.println("No se encontraron libros.");
+        } else {
+            System.out.println("Resultados:");
+            for (Libro libro : resultados) {
+                System.out.println(libro);
             }
         }
     }
